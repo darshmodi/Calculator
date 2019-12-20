79679f92610f8eb75d9fc472cc66fd7a5407b535
darshmodi <54471846+darshmodi@users.noreply.github.com>
2019-12-20 11:06:22 +0530

    All files added except node_modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import App from "./App";
+var destination= document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <App/>
+    </div>,destination
+);
